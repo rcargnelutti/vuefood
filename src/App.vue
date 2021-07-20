595f273add5108128cd5d2fbe9f38e48479ec7f8
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2021-07-20 00:28:01 -0300

    two commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App - UPDATE"/>
+  <HelloWorld msg="Welcome to Your Vue.js App - UPDATE 2"/>
 </template>
 
 <script>
