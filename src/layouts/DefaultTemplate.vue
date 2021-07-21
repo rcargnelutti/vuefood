5a548dbd2c62eb02bcd251dd52c4f7d26e8f7cb5
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2021-07-21 18:58:02 -0300

    Modulo 02 - Aula 02 - Organizar Template do VueFood

--- src/layouts/DefaultTemplate.vue
+++ src/layouts/DefaultTemplate.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <ul>
+        <!-- <ul>
             <li>
                 <router-link :to="{name: 'home'}">Home</router-link>
             </li>
@@ -10,16 +10,29 @@
             <li>
                 <router-link :to="{name: 'cart'}">Carrinho</router-link>
             </li>
-        </ul>
-        <router-view></router-view>
+        </ul> -->
+        <header-component/>
+
+        <!-- Page Content -->
+        <div class="container container-body">
+            <router-view></router-view>
+        </div> <!-- Page Content -->
+
+        <footer-component/>
     </div>
 </template>
 
 <script>
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import HeaderComponent from './_partials/header.vue'
+import FooterComponent from './_partials/footer.vue'
 
 export default {
+    components: {
+        HeaderComponent,
+        FooterComponent
+    }
     
 }
 </script>
