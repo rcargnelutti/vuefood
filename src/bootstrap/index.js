e50024218670ea536da85566f6b32d804bb5fa1b
Rodrigo Cargnelutti <cargnelutti@gmail.com>
2021-07-27 15:32:16 -0300

    Modulo 03 - Aula 02

--- src/bootstrap/index.js
+++ src/bootstrap/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 //const axios = require('axios').default;
-import { URI_API_BASE } from '../configs/api'
+import { URI_BASE_API } from '../configs/api'
 
-//axios.defaults.baseURL = URI_BASE_API
+axios.defaults.baseURL = URI_BASE_API
